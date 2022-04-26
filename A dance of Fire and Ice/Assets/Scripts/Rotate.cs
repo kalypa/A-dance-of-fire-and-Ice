3e9844ed8c1d47a1903ed0fc97847381f4d40f63
moonkanghyuck <moonkanghyuck@gmail.com>
2022-04-27 07:55:23 +0900

    얼불춤 회전 변경

--- A dance of Fire and Ice/Assets/Scripts/Rotate.cs
+++ A dance of Fire and Ice/Assets/Scripts/Rotate.cs
@@ -16,66 +16,48 @@ public class Rotate : MonoBehaviour
     [SerializeField]
     private float angle = 0f; //아이스가 파이어 바라보는 각도
 
-    [SerializeField]
-    private float fireRotate = 0f;
-    [SerializeField]
-    private float iceRotate = 0f;
-    [SerializeField]
-    private float rotating;
     private Transform rotateBall;
     private Transform stopBall;
     private bool isFireMove = true; //파이어가 회전하고 있다
-    void Update()
+
+	private void Start()
+    {
+        stopBall = Ice;
+        rotateBall = Fire;
+    }
+
+	void Update()
     {
         RotateMove();
-        AngleSet();
         if(Input.GetMouseButtonDown(0))
         {
+            //움직여야하는 공의 각도를 반대 방향으로 바꾼다
+            angle -= Mathf.PI;
+
             isFireMove = !isFireMove;
-            //움직여야하는 공의 rotate를 변경하다
-            Vector2 ballAngle = stopBall.position - rotateBall.position;
-            ballAngle = ballAngle.normalized;
-        }
+
+			if (isFireMove)
+			{
+				stopBall = Ice;
+				rotateBall = Fire;
+			}
+			else
+			{
+				stopBall = Fire;
+				rotateBall = Ice;
+			}
+		}
     }
 
     void RotateMove()
     {
         Vector2 resultPos = Vector2.zero;
-        Transform rotateTrm = null;
-        if (isFireMove)
-        {
-            //파이어가 움직일 때
-            rotateTrm = Fire;
-            resultPos = Ice.position;
-            fireRotate += Time.deltaTime;
-            resultPos.y -= Mathf.Sin(fireRotate * rotateSpd) * rotateDistance;
-            resultPos.x += Mathf.Cos(fireRotate * rotateSpd) * rotateDistance;
-            rotateBall = Fire;
-            stopBall = Ice;
-            rotating = fireRotate;
-        }
-        else
-        {
-            //아이스가 움직일 때 
-            rotateTrm = Ice;
-            resultPos = Fire.position;
-            iceRotate += Time.deltaTime;
-            resultPos.y -= Mathf.Sin(iceRotate * rotateSpd) * rotateDistance;
-            resultPos.x += Mathf.Cos(iceRotate * rotateSpd) * rotateDistance;
-            rotateBall = Ice;
-            stopBall = Fire;
-            rotating = iceRotate;
-        }
+        resultPos = stopBall.position;
+        angle += Time.deltaTime * rotateSpd;
+        resultPos.y -= Mathf.Sin(angle) * rotateDistance;
+        resultPos.x += Mathf.Cos(angle) * rotateDistance;
 
         //연산이 끝난 후의 위치를 적용시킨다.
-        rotateTrm.position = resultPos;
-    }
-
-    void AngleSet()
-    {
-        Vector2 angleVec = stopBall.position - rotateBall.position;
-        angleVec = angleVec.normalized;
-        angle = Mathf.Atan2(angleVec.y, angleVec.x) * Mathf.Rad2Deg;
-        
+        rotateBall.position = resultPos;
     }
 }
